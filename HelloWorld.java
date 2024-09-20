95998db9704f4b6a56da3ecc96cc2db0b75ffe3c
Tiknock <108576415+Tiknock@users.noreply.github.com>
2024-09-20 11:10:12 +0200

    CLose ISSUE #3

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,7 @@ import java.awt.*;
 public class HelloWorld {
     public static void main(String[] args) {
         JFrame frame = new JFrame(Constantes.HELLO_WORLD_TITLE);
-        frame.setSize(400, 400);
+        frame.setSize(1000, 1000);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JLabel label = new JLabel(Constantes.HELLO_WORLD_TEXT, SwingConstants.CENTER);
