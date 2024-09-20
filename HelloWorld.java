5a76235512dc054703e3e05ada64527ef1e95262
Tiknock <108576415+Tiknock@users.noreply.github.com>
2024-09-20 10:44:31 +0200

    C20 : Modification directe sur GitHub
    BG COLOR

--- HelloWorld.java
+++ HelloWorld.java
@@ -9,7 +9,7 @@ public class HelloWorld {
 
         JLabel label = new JLabel(Constantes.HELLO_WORLD_TEXT, SwingConstants.CENTER);
         label.setOpaque(true);
-        label.setBackground(Color.GREEN); // Changement du fond en vert
+        label.setBackground(Color.RED); // Changement du fond en vert
         label.setForeground(Color.BLACK); // Ajout de cette ligne pour mettre le texte en bleu
 
         frame.add(label);
